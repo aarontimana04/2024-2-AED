3f870e22e3c08d7cea6847b1073dee423cfa0747
aarontimana04 <112721918+aarontimana04@users.noreply.github.com>
2024-09-27 21:45:54 -0500

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -6,24 +6,20 @@ template <typename T>
 struct Nodo {
     T valor;
     Nodo<T>* next;
-    Nodo<T>* tail;
-    Nodo() : valor(0), next(nullptr),tail(nullptr){};
-    Nodo(const T& val) : valor(val), next(nullptr), tail(nullptr){};
+    Nodo() : valor(0), next(nullptr){};
+    Nodo(const T& val) : valor(val), next(nullptr){};
 };
 
 template <typename T>
-class iter_queue{
+class iter_queue {
     private:
     Nodo<T>* head;
+    Nodo<T>* tail;
     public:
-    iter_queue() : head(nullptr){};
+    iter_queue() : head(nullptr), tail(nullptr){};
 
     bool empty(){
-        if(head == nullptr){
-            return true;
-        } else {
-            return false;
-        }
+        return head == nullptr;
     }
 
     int size(){
@@ -33,35 +29,100 @@ class iter_queue{
             contador++;
             temp = temp->next;
         }
-
-        if(head == nullptr){
-            return 0;
-        }
         return contador;
     }
 
     void push(Nodo<T>* nnodo){
-        if(head == nullptr){
+        if (head == nullptr) {
             head = nnodo;
-            head->tail = nnodo;
+            tail = nnodo;
         } else {
-            head->tail->next = nnodo;
-            head->tail = nnodo;
+            tail->next = nnodo;
+            tail = nnodo;
         }
     }
 
     Nodo<T>* front(){
-        return head;
+        if (empty() == true) {
+            cout << "Queue vacía." << endl;
+            return nullptr;
+        } else {
+            return head;
+        }
     }
 
     Nodo<T>* back(){
-        return head->tail;
+        if (empty() == true) {
+            cout << "Queue vacía." << endl;
+            return nullptr;
+        } else {
+            return tail;
+        }
     }
 
     void pop(){
+        if (empty() == true) {
+            cout << "Queue vacía." << endl;
+            return;
+        }
+
         Nodo<T>* temp = head;
         head = head->next;
-        delete temp;        
+        if (head == nullptr) {
+            tail = nullptr;
+        }
+        delete temp;
     }
 
+    void print(){
+        Nodo<T>* temp = head;
+        while(temp != nullptr){
+            cout << temp->valor << " ";
+            temp = temp->next;
+        }
+        if (head == nullptr)
+            cout << "Queue vacío";
+
+        cout << endl;
+    }
 };
+
+int main() {
+    iter_queue<int> cola;
+    Nodo<int>* n1 = new Nodo<int>(10);
+    Nodo<int>* n2 = new Nodo<int>(11);
+    Nodo<int>* n3 = new Nodo<int>(12);
+    Nodo<int>* n4 = new Nodo<int>(13);
+
+    cola.push(n1);
+    cola.push(n2);
+    cola.push(n3);
+    cola.push(n4);
+
+    cola.print();
+
+    cout << cola.size() << endl;
+    cout << cola.empty() << endl;
+
+    cola.pop();
+    cola.print();
+    cout << cola.size() << endl;
+
+    Nodo<int>* n7 = cola.front();
+    if (n7 != nullptr) {
+        cout << "Front: " << n7->valor << endl;
+    }
+
+    cola.pop();
+    cola.pop();
+    cola.print();
+
+    Nodo<int>* n8 = cola.back();
+    if (n8 != nullptr) {
+        cout << "Back: " << n8->valor << endl;
+    }
+
+    
+
+    return 0;
+}
