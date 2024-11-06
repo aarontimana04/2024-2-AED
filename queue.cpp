c4fdf20604069d18531991857f5b32e85b539f88
aarontimana04 <112721918+aarontimana04@users.noreply.github.com>
2024-11-06 01:18:07 -0500

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -1,49 +1,54 @@
 #include <iostream>
-
 using namespace std;
 
 template <typename T>
 struct Nodo {
     T valor;
-    Nodo<T>* next;
-    Nodo() : valor(0), next(nullptr){};
-    Nodo(const T& val) : valor(val), next(nullptr){};
+    Nodo<T>* siguiente;
+    Nodo() : valor(T()), siguiente(nullptr) {};
+    Nodo(const T& val) : valor(val), siguiente(nullptr) {};
 };
 
 template <typename T>
 class iter_queue {
-    private:
-    Nodo<T>* head;
-    Nodo<T>* tail;
+private:
+    Nodo<T>* cabeza;
+    Nodo<T>* cola;
 
-    public:
-    iter_queue() : head(nullptr), tail(nullptr){}
+public:
+    iter_queue() : cabeza(nullptr), cola(nullptr) {}
+
+    ~iter_queue() {
+        while (!empty()) {
+            pop();
+        }
+    }
 
     // Verifica si la cola está vacía
     bool empty() const {
-        return head == nullptr;
+        return cabeza == nullptr;
     }
 
     // Devuelve el tamaño de la cola
-    int size() const {
-        Nodo<T>* temp = head;
+    int tamanio() const {
+        Nodo<T>* temp = cabeza;
         int contador = 0;
-        while(temp != nullptr) {
+        while (temp != nullptr) {
             contador++;
-            temp = temp->next;
+            temp = temp->siguiente;
         }
         return contador;
     }
 
     // Inserta un valor en la cola
     void push(const T& valor) {
-        Nodo<T>* nnodo = new Nodo<T>(valor); // Crea un nuevo nodo con el valor
-        if (head == nullptr) {
-            head = nnodo;
-            tail = nnodo;
+        Nodo<T>* nnodo = new Nodo<T>(valor);
+        if (cabeza == nullptr) {
+            cabeza = nnodo;
+            cola = nnodo;
         } else {
-            tail->next = nnodo;
-            tail = nnodo;
+            cola->siguiente = nnodo;
+            cola = nnodo;
         }
     }
 
@@ -53,7 +58,7 @@ class iter_queue {
             cout << "Queue vacía." << endl;
             return nullptr;
         } else {
-            return head;
+            return cabeza;
         }
     }
 
@@ -63,7 +68,7 @@ class iter_queue {
             cout << "Queue vacía." << endl;
             return nullptr;
         } else {
-            return tail;
+            return cola;
         }
     }
 
@@ -74,13 +79,12 @@ class iter_queue {
             return;
         }
 
-        Nodo<T>* temp = head;
-        head = head->next;
-        if (head == nullptr) {
-            tail = nullptr;
+        Nodo<T>* temp = cabeza;
+        cabeza = cabeza->siguiente;
+        if (cabeza == nullptr) {
+            cola = nullptr;
         }
         delete temp;
-        temp = nullptr; // Asegurar que el puntero se borra
     }
 
     // Imprime la cola
@@ -89,54 +93,11 @@ class iter_queue {
             cout << "Queue vacía" << endl;
             return;
         }
-        Nodo<T>* temp = head;
-        while(temp != nullptr) {
+        Nodo<T>* temp = cabeza;
+        while (temp != nullptr) {
             cout << temp->valor << " ";
-            temp = temp->next;
+            temp = temp->siguiente;
         }
         cout << endl;
     }
 };
-
-int main() {
-    iter_queue<int> cola;
-
-    // Insertando valores en la cola
-    cola.push(10);
-    cola.push(11);
-    cola.push(12);
-    cola.push(13);
-
-    // Imprimir cola
-    cola.print();
-
-    // Tamaño de la cola
-    cout << "Tamaño: " << cola.size() << endl;
-
-    // Verificar si está vacía
-    cout << "¿Cola vacía?: " << cola.empty() << endl;
-
-    // Eliminar el primer elemento
-    cola.pop();
-    cola.print();
-    cout << "Tamaño: " << cola.size() << endl;
-
-    // Obtener el frente de la cola
-    Nodo<int>* n7 = cola.front();
-    if (n7 != nullptr) {
-        cout << "Front: " << n7->valor << endl;
-    }
-
-    // Eliminar más elementos
-    cola.pop();
-    cola.pop();
-    cola.print();
-
-    // Obtener el último elemento (back) de la cola
-    Nodo<int>* n8 = cola.back();
-    if (n8 != nullptr) {
-        cout << "Back: " << n8->valor << endl;
-    }
-
-    return 0;
-}
